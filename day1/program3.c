48fcbd577ba6bdb10563f2fac455dcab767df13c
khushi kushwaha <25130fw003@gmailglb.ac.in>
2026-06-04 09:21:06 +0530

    day1 assignment is uploaded

---
+++ day1/program3.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+int main(){
+int n , i , fact = 1;
+printf("enter an integer:");
+    scanf("%d" , &n);
+    if(n < 0 ){
+        printf("error");
+    }
+    else{
+        for( i = 1; i <= n; ++i){
+            fact *= i;
+        }
+        printf("factorial of %d = %d" , n ,fact );
+
+    }
+    return 0;
+}
